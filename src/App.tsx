53242802feea69e41b46618f3acdc505ea23b99b
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2024-10-06 20:29:41 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home'));
+const Project = lazy(() => import('./pages/Project'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={<div className="container">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project/:slug" element={<Project />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
