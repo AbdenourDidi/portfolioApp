ce39d21088776168d9be4f23e16856c524180b67
AbdenourDidi <abdenour.didi@student.vinci.be>
2024-10-07 21:32:31 +0200

    start hero banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
-
+import styled from "styled-components";
+const HeroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Home = () => {
   return (
-    <div>
+    <HeroContainer>
       <Hero />
-    </div>
+    </HeroContainer>
   );
 };
 
