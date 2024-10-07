205b233dd401e0cb1fc6d8ec31eec4b1724e788c
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2024-10-07 21:50:05 +0200

    fix for didi

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,42 +3,41 @@ import profil from "../assets/IMG_20210430_002834_826.jpg";
 import styled from "styled-components";
 const HeroContainer = styled.div`
   display: flex;
+  flex-direction: row;
 `;
-const HeroP = styled.div`
+const DescriptionContainer = styled.div`
   flex-direction: column;
   align-items: flex-start;
   display: flex;
   flex: 1;
+  padding: 10px;
+  padding-right: 150px;
 `;
 
-const HeroImage = styled.div`
-  flex-direction: column;
-  align-items: flex-start;
+const ImageContainer = styled.div`
+  align-items: center;
+  justify-content: center;
   display: flex;
   flex: 1;
 `;
 
 const ImageRond = styled.div`
-  border-radius: 100px;
+  /* border-radius: 100px; */
   background-color: red;
   overflow: hidden;
   width: 150px;
   height: 150px;
 `;
 
-// const HeroH1 = styled.div`
-//   flex-direction: column;
-//   align-items: center;
-// `;
 const Hero = () => {
   return (
     <HeroContainer>
-      <HeroImage>
+      <ImageContainer>
         <ImageRond>
-          <img src={profil} alt="photo de profil" />
+          <img src={profil} alt="photo de profil" style={{ transform: `translate(-380px, -400px)` }} />
         </ImageRond>
-      </HeroImage>
-      <HeroP>
+      </ImageContainer>
+      <DescriptionContainer>
         <h1>Abdenour didi</h1>
         <p>
           Diplômé en informatique de gestion de l'Haute École Léonard de Vinci,
@@ -49,7 +48,7 @@ const Hero = () => {
           projets innovants tout en continuant à apprendre et à évoluer dans ce
           domaine en constante évolution.
         </p>
-      </HeroP>
+      </DescriptionContainer>
     </HeroContainer>
   );
 };
