7da95e0b815c7076464d2600d65e3f1bb3dc99a5
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2024-11-03 16:36:00 +0100

    fix containers

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,57 +10,32 @@ import linkedinWhite from "../assets/LinkedinBlanc.svg";
 const ContactContainer = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  gap: 31px;
 `;
 
-const LinkContainerLeft = styled.div`
-  flex-direction: column;
-  align-items: flex-start;
-  text-align: center;
-  display: flex;
-  flex: 1;
-  padding-left: 500px;
-`;
-const LinkContainerMedium = styled.div`
-  flex-direction: column;
-  align-items: center;
-  display: flex;
-  flex: 1;
-`;
-const LinkContainerRight = styled.div`
-  flex-direction: column;
-  align-items: flex-end;
-  display: flex;
-  flex: 1;
-  padding-right: 500px;
-`;
 
 const Contact = () => {
   return (
     <ContactContainer>
-      <LinkContainerLeft>
-        <HyperTextButton
-          imgBlack={gitBlack}
-          imgWhite={gitWhite}
-          href="https://github.com/AbdenourDidi"
-          text="Mon GitHub"
-        />
-      </LinkContainerLeft>
-      <LinkContainerMedium>
-        <HyperTextButton
-          imgBlack={linkedinBlack}
-          imgWhite={linkedinWhite}
-          href="https://www.linkedin.com/in/abdenour-didi-b68509326/"
-          text="Mon Linkedin"
-        />
-      </LinkContainerMedium>
-      <LinkContainerRight>
-        <HyperTextButton
-          imgBlack={mailBlack}
-          imgWhite={mailWithe}
-          href="mailto:abdenourdidi@hotmail.fr"
-          text="Mon Mail"
-        />
-      </LinkContainerRight>
+      <HyperTextButton
+        imgBlack={gitBlack}
+        imgWhite={gitWhite}
+        href="https://github.com/AbdenourDidi"
+        text="Mon GitHub"
+      />
+      <HyperTextButton
+        imgBlack={linkedinBlack}
+        imgWhite={linkedinWhite}
+        href="https://www.linkedin.com/in/abdenour-didi-b68509326/"
+        text="Mon Linkedin"
+      />
+      <HyperTextButton
+        imgBlack={mailBlack}
+        imgWhite={mailWithe}
+        href="mailto:abdenourdidi@hotmail.fr"
+        text="Mon Mail"
+      />
     </ContactContainer>
   );
 };
