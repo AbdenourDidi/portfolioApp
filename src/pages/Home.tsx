97616b82f63c046db3e17f32a0be0502e57d931f
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2024-11-03 16:06:52 +0100

    Revert "add svg for button"
    This reverts commit b3aa0a9843b3a65ea99bcbae61f946de72e6c9e8.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,15 @@
-import Bento from '../components/Bento'
-import styled from 'styled-components'
-import Grid from '../components/Grid'
-import GridItem from '../components/GridItem'
-import PageContainer from '../components/PageContainer'
 import React from "react";
 import Hero from "../components/Hero";
-
-const Test = styled.div`
-  justify-content: center;
-  align-items: center;
-  display: flex;
-`;
-
-const Width = styled.div`
-  width: 100%;
-  height: 100px;
-`;
-
-const HeroContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
+import Contact from "../components/Contact";
+import styled from "styled-components";
+const HeroContainer = styled.div``;
 const Home = () => {
-
   return (
-    <PageContainer>
-      <HeroContainer>
-        <Hero />
-      </HeroContainer>
-      <Bento>test2</Bento>
-      <Bento>test2</Bento>
-    </PageContainer>
-  )
+    <HeroContainer>
+      <Hero />
+      <Contact />
+    </HeroContainer>
+  );
 };
 
 export default Home;
