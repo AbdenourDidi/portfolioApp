72603131e5545d9153f93fdc4111b62c8f54911e
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2024-11-24 16:13:23 +0100

    naviagtions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import PageContainer from './components/PageContainer';
 
 const Home = lazy(() => import('./pages/Home'));
-const Project = lazy(() => import('./pages/Project'));
+const Projects = lazy(() => import('./pages/Projects'));
+const Skills = lazy(() => import('./pages/Skills'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
-    <>
+    <PageContainer>
       <Suspense fallback={<div className="container">Loading...</div>}>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project/:slug" element={<Project />} />
+          <Route path="/projects/" element={<Projects />} />
+          <Route path="/projects/:slug" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
-    </>
+    </PageContainer>
   );
 }
 
