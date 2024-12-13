1ea2c0d144f4327d839a26311bfe07b617b5dc0a
AbdenourDidi <abdenour.didi@student.vinci.be>
2024-12-13 02:01:39 +0100

    debug image for project details

--- src/components/ProjectImage.tsx
+++ src/components/ProjectImage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 interface Props {
@@ -27,21 +27,42 @@ const Title = styled.p`
 `;
 
 const ProjectImage = ({ title, images }: Props) => {
+  const [imageWidths, setImageWidths] = useState<{
+    [key: string]: string | number;
+  }>({});
+
+  useEffect(() => {
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+      img.onload = () => {
+        setImageWidths((prev) => {
+          let newWidth;
+          if (img.naturalHeight > img.naturalWidth) {
+            newWidth = `${img.naturalWidth}px`; // Portrait : largeur réelle
+          } else {
+            newWidth = "100%"; // Paysage : largeur à 100%
+          }
+          return {
+            ...prev,
+            [image]: newWidth,
+          };
+        });
+      };
+    });
+  }, [images]);
+
   return (
     <Container>
       <Title>{title}</Title>
       <ImagesContainer>
-        {images.map((image) => {
-          const img = new Image();
-          let width: number | string | undefined = "100%";
-          img.src = image;
-
-          if (img.naturalHeight > img.naturalWidth) {
-            width = img.naturalWidth;
-            console.log("hello");
-          }
-          return <img width={width} src={image} />;
-        })}
+        {images.map((image) => (
+          <img
+            width={imageWidths[image] || "100%"} // Utilise la largeur calculée ou "100%" par défaut
+            src={image}
+            alt="Project"
+          />
+        ))}
       </ImagesContainer>
     </Container>
   );
